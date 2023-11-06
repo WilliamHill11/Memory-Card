d2f20cf77d16b213fba59574ed700fa5ce1db353
william hill <williamhill0831@Hotmail.com>
2023-11-06 16:56:06 -0500

    added in states

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,62 @@
-// import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { random } from 'lodash';
+// import image1 from './Pokemon IDs/1.png';
+// import image2 from './Pokemon IDs/2.png';
+// import image3 from './Pokemon IDs/3.png';
 
 function App() {
-  const fetchPokemon = () => {
-    const randomIdGenerator = random(1, 3);
-    fetch(`https:pokeapi.co/api/v2/pokemon/${randomIdGenerator}/`)
+  const [score, setScore] = useState(0);
+  const [display, setDisplay] = useState([]);
+  const [pokemon, setPokemon] = useState([
+    {
+      name: '',
+      id: '',
+      imageUrl: '',
+    },
+  ]);
+
+  // data.sprites.front_default;
+
+  const randomIdGenerator = random(1, 3);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomIdGenerator}/`)
       .then((res) => res.json())
-      .then((data) => console.log(data.name));
-  };
+      .then((data) => {
+        console.log(data);
+      });
+  });
 
   return (
     <div className="App">
       <div className="title">
         <h1>Pokemon Memory Card</h1>
-        <button onClick={fetchPokemon}>Click me</button>
-        <h2>{}</h2>
+        <button>Click me</button>
+        <div className="score-board">
+          <h2>Score: </h2>
+          <h2>High Score: </h2>
+        </div>
+        <div className="pokemons">
+          <div className="pokemon-cards">
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomIdGenerator}.png`}
+              alt=""
+            />
+          </div>
+          <div className="pokemon-cards">
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomIdGenerator}.png`}
+              alt=""
+            />
+          </div>
+          <div className="pokemon-cards">
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomIdGenerator}.png`}
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
